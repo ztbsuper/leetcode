6e2b53018ae0d4be6796cea23360e703b77bc930
Marvin <ztbsuper@gmail.com>
2016-01-13 22:55:16 +0800

    [tianbo] add comment

--- src/test/java/leetcode/Leetcode268.java
+++ src/test/java/leetcode/Leetcode268.java
@@ -6,6 +6,25 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+
+/*
+*
+*
+*
+
+
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+
+*
+*
+*
+* */
 public class Leetcode268 {
 
 
